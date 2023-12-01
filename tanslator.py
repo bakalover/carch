098fb2b0d2994dc1a668f7ea741f0446d32c6083
bakalover <kostya@shprenger.com>
2023-12-01 22:09:57 +0300

    Need to consider mapping strings and  Spec size

--- tanslator.py
+++ tanslator.py
@@ -1,17 +1,11 @@
+from ast import Global
 from enum import Enum
 import sys
-import string
-
+from typing import List
 from isa import Opcode
-import re
 
 
-def get_tokens(source: str):
-    tokens = []
-    source.split
-
-
-def get_top_exps(source: str) -> [str]:
+def get_top_exps(source: str) -> List[str]:
     top_exps = []
     l, r = 0, 0
     counter = 0
@@ -30,12 +24,8 @@ def get_top_exps(source: str) -> [str]:
     return top_exps
 
 
-def is_nested(exp: str) -> bool:
-    return exp[0] == '(' and exp[len(exp) - 1] == ')'
-
-
-def to_tokens(s_exp: str) -> [str]:
-    return s_exp.replace('(', ' ( ').replace(')', ' ) ').split()
+def to_tokens(source: str) -> List[str]:
+    return source.strip().replace('\n', '').replace('(', ' ( ').replace(')', ' ) ').split()
 
 
 def convert_to_lists(tokens: list):
@@ -63,32 +53,64 @@ jmp_stack = []
 
 const_counter: int = 0
 
+# Data =
+# | Global vars
+# | Anon Constants (enumed during translation)
+# | Data.Spec (buff-reg for 2-op commands)
+#
+# Global entry -> (name, value)
+# Const -> (number, value)
+# Data.Spec -> (value)
+#
+# Mapping to memory:
+# Global -> From beggining
+# Const -> Magic offset + number
+# Data.Spec -> End
+# Stack ???
 
-class Ret(Enum):
-    CONST = 1,
-    GLOB = 2,
-    SPEC = 3
+
+class Data(Enum):
+    Global = 1,
+    Const = 2,
+    Spec = 3
 
 
-def construct(s_exp: [str]):  # -> [Ret, int | str | None]:
+word = 16
+const_offset = 100 * word
+data = {Data.Global: {}, Data.Const: {}, Data.Spec: 0}
+
+
+def add_str(where: Data, var_name: str | None, val: str):
+    global const_counter
+    to_add = val[1:-1]
+    if where == Data.Global:
+        data[where][var_name] = to_add
+    else:
+        data[where][const_counter] = to_add   # for-loop for string
+        add_inst(Opcode.LOAD, const_counter)  # for-loop for string
+        const_counter += 1
+
+
+def construct(s_exp: List[str] | str):
     global icounter, jmp_stack, const_counter
     match s_exp[0]:
 
         case "define":
             assert len(s_exp) == 3, "Invalid var definition!"
-            data['glob'][s_exp[1]] = s_exp[2]  # Name and init value
-            # return [False, s_exp[1]]
+
+            # String def
+            if s_exp[2].startswith("\""):
+                add_str(Data.Global, s_exp[1], s_exp[2])
+
+            # Number def
+            else:
+                data[Data.Global][s_exp[1]] = s_exp[2]
 
         case "set":
             assert len(s_exp) == 3, "Invalid var modifying!"
-            assert data['glob'].get(s_exp[1]) != None, "Non-existing var!"
-            construct(s_exp[2]) # Set res to Acc
-            # case Ret.SPEC:
-            #     add_inst(Opcode.LOAD, 'spec')
-            # case Ret.CONST:
-            #     add_inst(Opcode.LOAD, construct(s_exp[2])[1])
-            # case Ret.GLOB:
-            #     add_inst(Opcode.LOAD, construct(s_exp[2])[1])
+            assert data[Data.Global].get(s_exp[1]) != None, "Non-existing var!"
+            construct(s_exp[2])
+            # "Value to set" in ACC after constructing
             add_inst(Opcode.STORE, s_exp[1])
 
         case "if":
@@ -96,46 +118,54 @@ def construct(s_exp: [str]):  # -> [Ret, int | str | None]:
             jmp_stack.append(icounter)
             add_inst(Opcode.JIL, None)
             construct(s_exp[2])
-            instr[jmp_stack.pop()][1] = icounter
+            instr[jmp_stack.pop()][1] = icounter + 1
             jmp_stack.append(icounter)
+            # Avoiding "Else"
             add_inst(Opcode.JMP, None)
             construct(s_exp[3])
             instr[jmp_stack.pop()][1] = icounter
 
         case "==":
             construct(s_exp[1])
-            add_inst(Opcode.STORE, 'spec') # Spec - buffer for 2-op commands
+            add_inst(Opcode.STORE, Data.Spec)
             construct(s_exp[2])
-            add_inst(Opcode.SUB, 'spec')
+            add_inst(Opcode.SUB, Data.Spec)
 
-        case _:  # Constants or vars
-            if s_exp[0].isnumeric():
-                data['const'][const_counter] = s_exp[0]
+        case _:  # Constants or vars to Load
+
+            # Number const
+            if str(s_exp).isnumeric():
+                data[Data.Const][const_counter] = s_exp
                 add_inst(Opcode.LOAD, const_counter)
                 const_counter += 1
-                # return [True, const_counter]
+
+            # String const
+            elif str(s_exp)[0].startswith("\""):
+                add_str(Data.Const, None, str(s_exp))
+
+            # Var to Load
             else:
-                assert data.get(s_exp[1]) != None, "Non-existing var!"
-                add_inst(Opcode.LOAD, s_exp[1])
+                assert data[Data.Global].get(
+                    str(s_exp)) != None, "Non-existing var!"
+                add_inst(Opcode.LOAD, s_exp)
 
 
+# Instructions -> (number, opcode, operand's addr | None)
 instr = {}
-data = {'glob': {}, 'const': {}, 'spec1': 0}
 
 
 def translate(source: str):
-    source = source.strip().replace('\n', '')
     model = convert_to_lists(to_tokens(source))
     for top_exp in model:
         construct(top_exp)
+    add_inst(Opcode.HALT, None)
     return instr, data
 
 
 def main(source_path, target_instr_path, target_data_path):
     with open(source_path, encoding="utf-8") as file:
         source = file.read()
-        source += ")"
-        source = "(" + source
+        source = "(" + source + ")"
         instr, data = translate(source)
 
         from pprint import pprint
